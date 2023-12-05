92cdfacb0a1c758da4d09bb80d7d8160f3057e92
Nikee Kumar <119424557+thechronickle@users.noreply.github.com>
2023-12-05 22:31:09 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,83 +1,122 @@
-import random
+def ConstBoard(board):
+    print("Current State Of Board : \n\n");
+    for i in range (0,9):
+        if((i>0) and (i%3)==0):
+            print("\n");
+        if(board[i]==0):
+            print("- ",end=" ");
+        if (board[i]==1):
+            print("O ",end=" ");
+        if(board[i]==-1):    
+            print("X ",end=" ");
+    print("\n\n");
 
-def print_board(board):
-    for row in board:
-        print(" | ".join(row))
-        print("-" * 9)
+#This function takes the user move as input and make the required changes on the board.
+def User1Turn(board):
+    pos=input("Enter X's position from [1...9]: ");
+    pos=int(pos);
+    if(board[pos-1]!=0):
+        print("Wrong Move!!!");
+        exit(0) ;
+    board[pos-1]=-1;
+
+def User2Turn(board):
+    pos=input("Enter O's position from [1...9]: ");
+    pos=int(pos);
+    if(board[pos-1]!=0):
+        print("Wrong Move!!!");
+        exit(0);
+    board[pos-1]=1;
+
+#MinMax function.
+def minimax(board,player):
+    x=analyzeboard(board);
+    if(x!=0):
+        return (x*player);
+    pos=-1;
+    value=-2;
+    for i in range(0,9):
+        if(board[i]==0):
+            board[i]=player;
+            score=-minimax(board,(player*-1));
+            if(score>value):
+                value=score;
+                pos=i;
+            board[i]=0;
+
+    if(pos==-1):
+        return 0;
+    return value;
+    
+#This function makes the computer's move using minmax algorithm.
+def CompTurn(board):
+    pos=-1;
+    value=-2;
+    for i in range(0,9):
+        if(board[i]==0):
+            board[i]=1;
+            score=-minimax(board, -1);
+            board[i]=0;
+            if(score>value):
+                value=score;
+                pos=i;
+ 
+    board[pos]=1;
 
 
-def initialize_board():
-    return [[" " for _ in range(3)] for _ in range(3)]
+#This function is used to analyze a game.
+def analyzeboard(board):
+    cb=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
+    for i in range(0,8):
+        if(board[cb[i][0]] != 0 and
+           board[cb[i][0]] == board[cb[i][1]] and
+           board[cb[i][0]] == board[cb[i][2]]):
+            return board[cb[i][2]];
+    return 0;
 
-def is_winner(board, player):
-    # Check rows, columns, and diagonals for a win
-    for i in range(3):
-        if all(board[i][j] == player for j in range(3)) or all(board[j][i] == player for j in range(3)):
-            return True
-    return all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3))
-
-
-def is_board_full(board):
-    return all(board[i][j] != " " for i in range(3) for j in range(3))
-
-
-def is_valid_move(board, move):
-    row, col = move
-    return 0 <= row < 3 and 0 <= col < 3 and board[row][col] == " "
-
-
-def make_move(board, move, player):
-    row, col = move
-    if is_valid_move(board, move):
-        board[row][col] = player
-        return True
-    return False
-
-
-def get_player_move():
-    try:
-        move = input("Enter row and column (e.g., '1 2'): ")
-        row, col = map(int, move.split())
-        return row, col
-    except (ValueError, IndexError):
-        print("Invalid input. Please enter two space-separated numbers.")
-        return get_player_move()
-
-
-def get_computer_move(board):
-    empty_cells = [(i, j) for i in range(3) for j in range(3) if board[i][j] == " "]
-    return random.choice(empty_cells) if empty_cells else None
-
-
-def play_tic_tac_toe():
-    board = initialize_board()
-    players = ["X", "O"]
-    current_player = players[0]
-
-    while True:
-        print_board(board)
-
-        if current_player == "X":
-            row, col = get_player_move()
-        else:
-            print("Computer's move:")
-            row, col = get_computer_move(board)
-
-        if make_move(board, (row, col), current_player):
-            if is_winner(board, current_player):
-                print_board(board)
-                print(f"{current_player} wins!")
-                break
-            elif is_board_full(board):
-                print_board(board)
-                print("It's a tie!")
-                break
+#Main Function.
+def main():
+    choice=input("Enter 1 for single player, 2 for multiplayer: ");
+    choice=int(choice);
+    #The broad is considered in the form of a single dimentional array.
+    #One player moves 1 and other move -1.
+    board=[0,0,0,0,0,0,0,0,0];
+    if(choice==1):
+        print("Computer : O Vs. You : X");
+        player= input("Enter to play 1(st) or 2(nd) :");
+        player = int(player);
+        for i in range (0,9):
+            if(analyzeboard(board)!=0):
+                break;
+            if((i+player)%2==0):
+                CompTurn(board);
             else:
-                current_player = players[1] if current_player == players[0] else players[0]
-        else:
-            print("Invalid move. Try again.")
+                ConstBoard(board);
+                User1Turn(board);
+    else:
+        for i in range (0,9):
+            if(analyzeboard(board)!=0):
+                break;
+            if((i)%2==0):
+                ConstBoard(board);
+                User1Turn(board);
+            else:
+                ConstBoard(board);
+                User2Turn(board);
+         
 
-
-if __name__ == "__main__":
-    play_tic_tac_toe()
+    x=analyzeboard(board);
+    if(x==0):
+         ConstBoard(board);
+         print("Draw!!!")
+    if(x==-1):
+         ConstBoard(board);
+         print("X Wins!!! Y Loose !!!")
+    if(x==1):
+         ConstBoard(board);
+         print("X Loose!!! O Wins !!!!")
+       
+#---------------#
+main()
+#---------------#
